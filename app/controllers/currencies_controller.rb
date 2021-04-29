422fec9789dcfe8a2a86359a37644435ab8a32c7
Hammad-Rahman <hammad.rahman@chicagobooth.edu>
2021-04-29 12:22:43 +0000

    Attempting to fix after inputting symbol hash

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -2,9 +2,11 @@ class CurrenciesController < ApplicationController
 
   def first_currency
     
-    raw_data = open("https://api.exchangerate.host/symbols").read
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash = @parsed_data.fetch("symbols")
 
-    @array_currencies = Array.new
+    @array_currencies = @symbols_hash.keys
 
     render({ :template => "currency_templates/step_one.html.erb"})
   end
