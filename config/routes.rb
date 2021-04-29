cfc3a48cc5b1b25ca80ac6c41e4cd2cc56c73eb8
Hammad-Rahman <hammad.rahman@chicagobooth.edu>
2021-04-29 12:43:11 +0000

    made second_step rcav

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
  
   get("/forex", {:controller => "currencies", :action => "first_currency"})
+  get("/forex/:first_currency", {:controller => "currencies", :action => "second_currency"})
 
 
 
